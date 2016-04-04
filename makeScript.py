3e6181fe19bac4ed2485b073dfd5c6e54cd630bb
atronah <atronah@medisys.ru>
2016-04-04 12:55:22 +0300

    add support custom script file
    and remove support custom encoding from .ini

--- makeScript.py
+++ makeScript.py
@@ -4,6 +4,10 @@ import os
 import re
 import argparse
 import configparser
+import datetime
+import subprocess
+import shlex
+import glob
 
 
 
@@ -63,48 +67,96 @@ def connectInfoHadler(inputFile, args):
 #                 with open(os.path.join(src, fName), 'r', encoding='utf-8') as script:
 #                     o.write(handler(script, args) + '\n')
 
-def scriptsContent(settings, group):
+
+def get_git_info(obj_path):
+    git_info = {}
+    
+    obj_path = os.path.abspath(obj_path)
+    workdir = os.path.abspath(os.path.dirname(obj_path))
+    while not os.path.ismount(workdir):
+        if os.path.isdir(os.path.join(workdir, '.git')):
+            for param, command in {'sha': 'git rev-parse HEAD'
+                                    , 'branch': 'git rev-parse --abbrev-ref HEAD'
+                                    , 'author': 'git log -1 --pretty="%an (%ae)" -- "{}"'.format(obj_path)
+                                    , 'date': 'git log -1 --pretty="%ad" -- "{}"'.format(obj_path)
+                                    , 'message': 'git log -1 --pretty=%B -- "{}"'.format(obj_path)
+                                    }.items():
+                #if param == 'message': print(command)
+                p = subprocess.Popen(shlex.split(command), stdout = subprocess.PIPE, cwd = workdir)
+                p.wait()
+                git_info[param] = ''.join([line.decode('utf-8') for line in p.stdout.readlines()]).replace('\n', ' ')
+            break
+        workdir = os.path.abspath(os.path.join(workdir, os.pardir))
+    return git_info
+
+
+def add_git_info(content, git_info):
+    if not git_info:
+        return content
+    first_begin = re.search(r'\bbegin\b', content, re.IGNORECASE)
+    if first_begin:
+        return content[:first_begin.end()] + '\n' \
+                    + '-- generated on ' + str(datetime.datetime.now()) + '\n' \
+                    + '-- git branch: ' + git_info['branch'] + '\n' \
+                    + '-- git SHA-1: ' + git_info['sha'] + '\n' \
+                    + '-- git author: ' + git_info['author'] + '\n' \
+                    + '-- git date: ' + git_info['date'] + '\n' \
+                    + '-- git commit message: ' + git_info['message'] + '\n' \
+                    + content[first_begin.end():]
+    return content
+    
+
+def fileContent(fname, encoding, params):
+    with open(fname, 'r', encoding=encoding) as f:
+        print('processing file: {}'.format(fname))
+        content = add_git_info(f.read(), get_git_info(fname))
+        try:
+            content = content.format(**params)
+            return content
+        except Exception as e:
+            print('error "{0}" occured during formating content of file: "{1}"'.format(e, fname))
+    
+
+def processScriptsGroup(settings, group, encoding):
     if settings.has_option(group, 'scripts'):
         for fname in settings[group]['scripts'].split('\n'):
             fname = fname.strip('"')
             if os.path.isfile(fname):
-                with open(fname, 'r', encoding=settings['general']['encoding']) as f:
-                    content = f.read()
-                    try:
-                        content = content.format(**settings[group])
-                        yield content
-                    except Exception as e:
-                        print('error "{0}" occured during formating content of file: "{1}"'.format(e, fname))
-
+                yield fileContent(fname, encoding, settings[group])
 
 
 def main():
+    encoding = 'utf-8'
+    
     parser = argparse.ArgumentParser(description='concatenates all scripts in one')
     parser.add_argument('-d', '--dir', dest='dir', default=None, help='directory with scripts')
     parser.add_argument('-o', '--out', dest='out', default=None, help='result file name')
     parser.add_argument('-s', '--settings', dest='settings', default='settings.ini', help='settings file')
     parser.add_argument('-g', '--groups', dest='groups', default='groups'
                         , help='option name which contains name of groups with used scripts')
+    parser.add_argument('files', nargs='*')
     args = parser.parse_args()
 
     if args.dir:
         os.chdir(args.dir)
 
-    settings = configparser.ConfigParser(defaults={'encoding': 'utf-8'})
+    settings = configparser.ConfigParser()
     settings.read(args.settings)
-
-    if not settings.has_option('general', args.groups):
-        return 1
 
     # print(settings['routines'])
     if args.out is None:
         args.out = args.groups + '.sql'
-
-    with open(args.out, 'w', encoding='utf-8') as o:
-        for group in settings['general'][args.groups].split(','):
-            group = group.strip()
-            for content in scriptsContent(settings, group):
-                o.write(content + '\n\n')
+    print(os.path.abspath(args.out))
+    
+    with open(args.out, 'w', encoding = encoding) as o:
+        if settings.has_option('general', args.groups):
+            for group in settings['general'][args.groups].split(','):
+                group = group.strip()
+                for content in processScriptsGroup(settings, group, encoding):
+                    o.write(content + '\n\n')
+        for fname_pattern in args.files:
+            for fname in glob.glob(fname_pattern):
+                o.write(fileContent(fname, encoding, {}) + '\n\n')
     return 0
 
 

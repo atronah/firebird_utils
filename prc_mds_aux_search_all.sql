a1e8a17f7169d077e2cc624377a327f3aa7ccaf2
atronah <atronah.ds@gmail.com>
2016-07-28 19:30:03 +0300

    adds script for search text in all tables

---
+++ prc_mds_aux_search_all.sql
@@ -0,0 +1,28 @@
+create or alter procedure mds_aux_search_all(
+    text blob sub_type text
+)
+returns (
+    ftable varchar(31)
+    , fname varchar(31)
+    , ftype varchar(31)
+    , cnt bigint
+)
+as
+begin
+    if(coalesce(:text, '') = '' ) then exit;
+    for select
+            trim(rdb$field_name) as fname
+            , trim(rdb$relation_name) as ftable
+            , trim(rdb$field_source) as ftype
+        from rdb$relation_fields
+        where rdb$field_source not containing '$'
+                and (rdb$field_source containing 'text'
+                    or rdb$field_source containing 'code'
+                    or rdb$field_source containing 'ident')
+    into fname, ftable, ftype do
+    begin
+        execute statement ('select count(*) from ' || :ftable || ' where upper(:text) = upper(cast("' || :fname || '" as varchar(16000)))') (text := :text)
+        into cnt;
+        if (cnt > 0) then suspend;
+    end
+end

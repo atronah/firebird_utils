1417825cc4a8db982cdfc2d0ae8cdc93bfed788a
atronah <atronah.ds@gmail.com>
2016-07-28 19:14:20 +0300

    adds procedure for obfuscare text (convert to ASCII codes splited by comma)

---
+++ prc_aux_obfuscare_text.sql
@@ -0,0 +1,30 @@
+create or alter procedure aux_obfuscare_text(
+    text blob sub_type text
+    , direction smallint = 0)
+returns (
+    result blob sub_type text
+)
+as
+declare variable code integer;
+declare variable pos integer;
+begin
+	result = '';
+
+	if (direction = 0) then
+		begin
+			pos = 1;
+			while (pos < char_length(text)+1) do
+			begin
+			result = result || ascii_val(substring(text from pos for 1)) || ',';
+			pos = pos + 1;
+			end
+		end
+	else
+		begin
+			for select part
+				from aux_split_text(:text, ',')
+				into :code
+				do result = result || ascii_char(code);
+		end
+	suspend;
+end

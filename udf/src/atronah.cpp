9d2a3bdab9b009601bb1922393054a47cc52f163
atronah <atronah.ds@gmail.com>
2016-09-12 18:12:50 +0300

    adds source file for common udf library

---
+++ udf/src/atronah.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <cstring>
+
+#include <Windows.h>
+#include <ib_util.h>
+
+extern "C" char* inflectName(char *name, int *c){
+    HINSTANCE hDLL;
+    typedef int (__stdcall *LPFNGetFIOPadegFSAS)
+      (unsigned char *, int, unsigned char *, int &);
+    LPFNGetFIOPadegFSAS lpfnGetFIOPadegFSAS;
+    char * inflected_name;
+    char cResult[1024];
+    int len = 1024;
+    int state;
+
+    // http://www.delphikingdom.ru/asp/viewitem.asp?UrlItem=/mastering/poligon/webpadeg.htm#SubHeader_1762079927060
+    hDLL = LoadLibrary("Padeg.dll");
+    inflected_name = (char*)ib_util_malloc(6);
+    if (hDLL != NULL){
+        lpfnGetFIOPadegFSAS = (LPFNGetFIOPadegFSAS)GetProcAddress(hDLL, "GetFIOPadegFSAS");
+        if (lpfnGetFIOPadegFSAS != NULL) {
+            /* Возможные результаты выполнения
+            0 — успешное завершение;
+            -1 — недопустимое значение падежа;
+            -2 — недопустимое значение рода;
+            -3 — размер буфера недостаточен для размещения результата преобразования ФИО.
+            */
+            state = lpfnGetFIOPadegFSAS((unsigned char *)name, *c, (unsigned char *)cResult, len);
+            if (state == 0) {
+                inflected_name = (char*)ib_util_malloc(len + 1);
+                strcpy(inflected_name, cResult);
+            }else{
+                sprintf(inflected_name, "%i (%i)", state, c);
+            }
+        }
+        FreeLibrary(hDLL);
+    }
+    return inflected_name;
+}

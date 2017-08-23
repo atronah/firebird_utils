0d9049fd6e24c36c8442950b54e0332cb2d7dca9
atronah <atronah.ds@gmail.com>
2017-08-23 17:00:59 +0300

    ft(statement_status): adds query to display sql-statement status

---
+++ sel_statement_status.sql
@@ -0,0 +1,11 @@
+select
+    s.mon$sql_text
+    , s.mon$statement_id
+    , rs.mon$record_seq_reads as non_indexed_reads
+    , rs.mon$record_idx_reads as indexed_reads
+    , rs.mon$record_inserts as inserts
+    , rs.mon$record_updates as updates
+    , rs.mon$record_deletes as updates
+from mon$statements as s
+inner join mon$record_stats as rs on rs.mon$stat_id = s.mon$stat_id
+where s.mon$sql_text containing :sql_part

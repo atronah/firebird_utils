021799eb3e8295828a93c773e5e9a97cc325c426
atronah <atronah.ds@gmail.com>
2017-12-05 19:53:34 +0300

    ft(aux_add_field): adds procedure to add table fields

---
+++ prc_mds_meta_add_field.sql
@@ -0,0 +1,32 @@
+set term ^ ;
+
+-- adds field into table if it is not exists
+create or alter procedure mds_meta_add_field(
+    table_name varchar(31)
+    , field_name varchar(31)
+    , field_type varchar(31)
+    , field_comment varchar(255) = null
+)
+as
+begin
+    table_name = trim(table_name);
+    field_name = trim(field_name);
+
+    if (not exists(select rdb$field_name
+                    from rdb$relation_fields
+                    where trim(rdb$relation_name) = upper(:table_name)
+                        and trim(rdb$field_name) = upper(:field_name)))
+    then
+    begin
+        execute statement 'alter table ' || table_name
+                            || ' add ' || field_name || ' ' || field_type
+                            -- without it fails when execute from scrip file (by `isql -i`) after creating table
+                            with autonomous transaction;
+    end
+    if (field_comment is not null)
+        then execute statement 'comment on column ' || table_name || '.' || field_name
+                                    || ' is ''' || field_comment || ''''
+                                    with autonomous transaction;
+end^
+
+set term ; ^

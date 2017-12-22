68762845476f38599fd1c1258159fd12769cbd39
atronah <atronah.ds@gmail.com>
2017-12-22 10:50:35 +0300

    rf(sel_stmt_status): adds attaachment_id, fixes alias

--- sel_statement_status.sql
+++ sel_statement_status.sql
@@ -2,11 +2,12 @@ select
     s.mon$sql_text
     , s.mon$timestamp
     , s.mon$statement_id
+    , s.mon$attachment_id
     , rs.mon$record_seq_reads as non_indexed_reads
     , rs.mon$record_idx_reads as indexed_reads
     , rs.mon$record_inserts as inserts
     , rs.mon$record_updates as updates
-    , rs.mon$record_deletes as updates
+    , rs.mon$record_deletes as deletes
 from mon$statements as s
 inner join mon$record_stats as rs on rs.mon$stat_id = s.mon$stat_id
 where s.mon$sql_text containing :sql_part

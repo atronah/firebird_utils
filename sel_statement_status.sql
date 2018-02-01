9d14b8e2ae414f6f1490a2fb6ec068f9628114f5
atronah <atronah.ds@gmail.com>
2018-02-01 18:03:32 +0300

    ft(sel_statements): adds out `delete from mon$statements` query

--- sel_statement_status.sql
+++ sel_statement_status.sql
@@ -8,6 +8,7 @@ select
     , rs.mon$record_inserts as inserts
     , rs.mon$record_updates as updates
     , rs.mon$record_deletes as deletes
+    , 'delete from mon$statements where mon$statement_id = ' || s.mon$statement_id as delete_stmt
 from mon$statements as s
 inner join mon$record_stats as rs on rs.mon$stat_id = s.mon$stat_id
 where s.mon$sql_text containing :sql_part

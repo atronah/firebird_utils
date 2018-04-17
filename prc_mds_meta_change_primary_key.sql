6a633b8bf92a898ea17249bda4ae48620b0e7f7c
atronah <atronah.ds@gmail.com>
2018-04-17 13:16:41 +0300

    ft(meta_change_primary_key): adds procedure to add/update primary key constraint

---
+++ prc_mds_meta_change_primary_key.sql
@@ -0,0 +1,55 @@
+set term ^ ;
+
+-- change primary key for table
+create or alter procedure mds_meta_change_primary_key(
+    table_name varchar(31)
+    , fields_list varchar(1024)
+    , constraint_name varchar(31) = null
+    , no_change_constraint_name smallint = 1 -- if more than 0 do nothing for primary key which differs only by name
+)
+as
+declare index_name varchar(31);
+declare existed_constraint_name varchar(31);
+declare existed_fields_list varchar(1024);
+declare field_name varchar(31);
+begin
+    table_name = trim(table_name);
+    constraint_name = trim(coalesce(constraint_name, left('pk_' || table_name, 31)));
+    select list(part) from aux_split_text(:fields_list, ',', 1) into fields_list;
+    if (coalesce(fields_list, '') = '') then exit;
+
+    -- get info about existing primary key
+    select trim(cons.rdb$constraint_name)
+            , list(trim(seg.rdb$field_name)) as current_fields_list
+        from rdb$relation_constraints as cons
+            inner join rdb$index_segments as seg on seg.rdb$index_name = cons.rdb$index_name
+        where trim(upper(cons.rdb$relation_name)) = upper(:table_name)
+            and trim(upper(cons.rdb$constraint_type)) = upper('primary key')
+        group by 1
+        into existed_constraint_name, existed_fields_list;
+
+    -- (re)create primary key constraint if new constraint is different (by fields or name)
+    if (upper(fields_list) <> upper(existed_fields_list)
+        or (upper(constraint_name) <> upper(existed_constraint_name) and no_change_constraint_name = 0)
+    ) then
+    begin
+        -- remove existed constraint
+        if (coalesce(existed_constraint_name, '') <> '')
+            then execute statement 'alter table ' || table_name || ' drop constraint ' || existed_constraint_name;
+
+        -- set NOT NULL for fields which doesn't have it yet
+        for select part
+            from aux_split_text(:fields_list, ',', 1) as ast
+                inner join rdb$relation_fields as rf on trim(upper(rf.rdb$relation_name)) = upper(:table_name)
+                                                        and trim(upper(rf.rdb$field_name)) = upper(trim(ast.part))
+            where coalesce(rdb$null_flag, 0) <> 1
+            into field_name
+            do execute statement 'alter table ' || table_name || ' alter ' || field_name || ' SET NOT NULL';
+
+        execute statement 'alter table ' || table_name
+                            || ' add constraint ' || constraint_name
+                            || ' primary key (' || :fields_list ||  ')';
+    end
+end^
+
+set term ; ^

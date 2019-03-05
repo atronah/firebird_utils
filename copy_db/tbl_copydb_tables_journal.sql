59c71dfa6d8faeb996a993784493e1a05d4a344b
atronah <atronah.ds@gmail.com>
2019-03-05 16:15:46 +0300

    fixup! ft(copy_db): adds copydb_tables_journal table

--- copy_db/tbl_copydb_tables_journal.sql
+++ copy_db/tbl_copydb_tables_journal.sql
@@ -2,16 +2,16 @@ create sequence copydb_tables_journal_seq;
 
 create table copydb_tables_journal (
     id bigint
-    , table_name varchar(31)
+    , name varchar(31)
     , total_records bigint
     , copied_records bigint
     , skipped_records bigint
     , status smallint
     , processed timestamp
     , info varchar(1024)
-    , constraint pk_copydb_tables_journal primary key (id);
+    , constraint pk_copydb_tables_journal primary key (id)
 );
 
-create unique index copydb_tables_journal_table_name on copydb_tables_journal(table_name);
+create unique index copydb_tables_journal_name on copydb_tables_journal computed by (upper(name));
 
-comment on column copydb_log.status is '0 - Успешно скопировано; 1 - В процессе копирования; 99 - Ошибка в процессе копирования';
+comment on column copydb_tables_journal.status is '0 - Успешно скопировано; 1 - В процессе копирования; 99 - Ошибка в процессе копирования';

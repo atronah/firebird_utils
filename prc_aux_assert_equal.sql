92048b718b2fb1262f1be05508e7c5eb790e5c23
atronah <atronah.ds@gmail.com>
2019-04-09 12:28:00 +0300

    fx(aux_asser_equal): fixes no exception for comparison ` ` and  `     `
    bug was cause firebird thinks that empty string `` equals (not distinct from) non-wmpty string with only spaces

--- prc_aux_assert_equal.sql
+++ prc_aux_assert_equal.sql
@@ -10,7 +10,8 @@ create or alter procedure aux_assert_equal(
 as
 declare error_message varchar(4096);
 begin
-    if (left_value is distinct from right_value)
+    if (left_value is distinct from right_value
+            or char_length(left_value) is distinct from char_length(right_value))
         then exception AUX_ASSERT_EXCEPTION  coalesce(message_prefix, '')
                                                 || 'Value `' || coalesce(left_value, 'null')
                                                 || '` is not equal to value `'
@@ -19,7 +20,7 @@ end^
 
 set term ; ^
 
-comment on procedure aux_assert_equal is 'Procedure for equality test of two values and throwing exception if they don''t equal'; 
+comment on procedure aux_assert_equal is 'Procedure for equality test of two values and throwing exception if they don''t equal';
 comment on parameter aux_assert_equal.left_value is 'Left/first value to comparison';
 comment on parameter aux_assert_equal.right_value is 'Right/second value to comparison';
 comment on parameter aux_assert_equal.message_prefix is 'Prefix for error message';

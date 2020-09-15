1fc1d3c6a657ea1dc7f1457fc011b4d20d9f46cb
atronah <atronah.ds@gmail.com>
2020-09-15 15:38:21 +0300

    rf(sel_statement_status): deletes trailing spaces

--- sel_statement_status.sql
+++ sel_statement_status.sql
@@ -8,11 +8,11 @@ select
     rs.mon$record_inserts as inserts,
     rs.mon$record_updates as updates,
     rs.mon$record_deletes as deletes,
-    
+
     s.mon$transaction_id as trans_id,
     s.mon$attachment_id as atach_id,
     right(a.mon$remote_process, 32) as process_name,
-    
+
     rs.mon$record_backouts as "Records Backed Out",
     rs.mon$record_purges as "Records Purged",
     rs.mon$record_expunges as "Records Expunged",
@@ -20,14 +20,14 @@ select
     io.mon$page_writes as "Page Writes",
     io.mon$page_fetches as "Page Fetches",
     io.mon$page_marks as "Page Marks",
-    
+
     a.mon$remote_process as remote_process,
     'delete from mon$statements where mon$statement_id = ' || s.mon$statement_id as delete_stmt
 from mon$statements as s
     inner join mon$record_stats as rs on rs.mon$stat_id = s.mon$stat_id
     inner join mon$attachments as a on a.mon$attachment_id = s.mon$attachment_id
     left join mon$io_stats as io on io.mon$stat_id = s.mon$stat_id
-where 
+where
     -- Фильтр по содержимому запроса
     (cast(:sql_part as varchar(1024)) = '' or s.mon$sql_text containing :sql_part)
     -- фильтр "PID процесса на сервере Firebird"

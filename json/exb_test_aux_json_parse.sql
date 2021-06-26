406a4a6906723c2c9e92aa4ee6ca4b3591cfeee0
atronah <atronah.ds@gmail.com>
2021-06-26 04:52:42 +0300

    ft(json/test_parse): adds test for complex json

--- json/exb_test_aux_json_parse.sql
+++ json/exb_test_aux_json_parse.sql
@@ -54,6 +54,49 @@ begin
     suspend;
     -- -- -- --
     -- -- -- --
+
+    -- -- -- --
+    test_json = '   {
+                        "text_param": "text value",
+                        "array param": [
+                            "text array item 1",
+                            "text array item 2",
+                            "text array item 3",
+                        ],
+                        "object param": {
+                            "object text param": "child text value",
+                            "object num param": -932.45    ,
+                            "object obj param": {
+                                "mixed array": [
+                                    "just string",
+                                    140,
+                                    { "name" : "simple" },
+                                    "param": 0.98
+                                ],
+                            },
+                        },
+                    }';
+    test_name = 'complex: text_param';
+    -- left(4)|right(4)|node_path|node_index|node_type|node_name|node_content|error_code
+    expected_value = '"tex|lue"|/text_param|0|param|text_param|text value|0';
+    resulting_value = (select first 1
+                            substring(:test_json from start_pos for 4)
+                                || '|' || substring(:test_json from end_pos - 4 + 1 for 4)
+                                || '|' || node_path || '|' || node_index || '|' || node_type
+                                || '|' || node_name || '|' || node_content || '|' || error_code
+                            from aux_json_parse(:test_json));
+    test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
+    total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
+    suspend;
+
+    test_name = 'complex: count';
+    expected_value = 16;
+    resulting_value = (select count(*) from aux_json_parse(:test_json));
+    test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
+    total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
+    suspend;
+    -- -- -- --
+    -- -- -- --
 end^
 
 set term ; ^

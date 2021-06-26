3cb32e4dc70e33ce919a3fbe16ed07cef24e8b83
atronah <atronah.ds@gmail.com>
2021-06-26 04:52:28 +0300

    rf(json/test): changes names of test
    to match the pattern: <test json name>: <test name>

--- json/exb_test_aux_json_parse.sql
+++ json/exb_test_aux_json_parse.sql
@@ -36,14 +36,14 @@ begin
     -- -- -- --
     -- -- -- --
     test_json = '"text_param": "text value"';
-    test_name = 'count';
+    test_name = 'param: count';
     expected_value = 1;
     resulting_value = (select count(*) from aux_json_parse(:test_json));
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
 
-    test_name = 'fields';
+    test_name = 'param: values';
     -- start_pos|end_pos|node_path|node_index|node_type|node_name|node_content|error_code
     expected_value = '1|26|/text_param|0|param|text_param|text value|0';
     resulting_value = (select first 1

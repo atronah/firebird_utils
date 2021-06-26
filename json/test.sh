7a8011f63a794cadc212af19920b5d46da59177d
atronah <atronah.ds@gmail.com>
2021-06-26 03:11:29 +0300

    ft(json/test_parse): adds testing scripts (bash runner + sql-test)

---
+++ json/test.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+db_user=SYSDBA
+db_password=masterkey
+db_name=test.fdb
+
+rm -f test.fdb
+echo "create database '${db_name}' page_size 16384; commit;" | isql -user ${db_user} -pas ${db_password}
+
+isql -user ${db_user} -pas ${db_password} ${db_name} -i prc_aux_json_parse.sql
+isql -user ${db_user} -pas ${db_password} ${db_name} -i exb_test_aux_json_parse.sql
+
+rm -f test.fdb

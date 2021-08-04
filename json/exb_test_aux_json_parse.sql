f3aebfdc6e33d474e4528373ca659ea12c9b01c9
atronah <atronah.ds@gmail.com>
2021-08-04 14:09:33 +0300

    rf(json_test_aux_json_parse): reformat tests
    - changes names of the tests
    - moves `expected_value` after `resulting_value`
    - adds comments

--- json/exb_test_aux_json_parse.sql
+++ json/exb_test_aux_json_parse.sql
@@ -21,12 +21,18 @@ begin
     -- -- -- --
     -- -- -- --
     test_json = ASCII_CHAR(13) || ASCII_CHAR(10) || '   "just text" ';
-    test_name = 'just text';
-    -- node_start|node_end|node_path|node_index|value_type|name|val|error_code
-    expected_value = '6|16|/|0|string|null|just text|0';
+    test_name = 'parse simple text as json';
     resulting_value = (select first 1
-                             node_start || '|' || node_end || '|' || node_path || '|' || node_index || '|' || value_type || '|' || coalesce(name, 'null') || '|' || val || '|' || error_code
+                             node_start
+                            || '|' || node_end
+                            || '|' || node_path
+                            || '|' || node_index
+                            || '|' || value_type
+                            || '|' || coalesce(name, 'null')
+                            || '|' || val
+                            || '|' || error_code
                             from aux_json_parse(:test_json));
+    expected_value = '6|16|/|0|string|null|just text|0';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -36,14 +42,14 @@ begin
     -- -- -- --
     -- -- -- --
     test_json = '"text_param": "text value"';
-    test_name = 'param: count';
-    expected_value = 1;
+    test_name = 'number of all found json pairs "param:value" in json with only one object without trailing "{" and "}"';
     resulting_value = (select count(*) from aux_json_parse(:test_json));
+    expected_value = 1; -- expected number 1 because source json contains only one parameter
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
 
-    test_name = 'param: values';
+    test_name = 'start/end positions of found node, its path, type, index, name and value';
     -- node_start|node_end|node_path|node_index|value_type|name|val|error_code
     expected_value = '1|26|/|0|string|text_param|text value|0';
     resulting_value = (select first 1
@@ -72,14 +78,13 @@ begin
                         },
                     }';
     test_name = 'complex: some param';
-    -- left(4)|right(4)|node_path|node_index|value_type|name|val|error_code
-    expected_value = '"som|lue"|/-/|0|string|some param|text value|0';
     resulting_value = (select first 1
                             substring(:test_json from node_start for 4)
                                 || '|' || substring(:test_json from node_end - 4 + 1 for 4)
                                 || '|' || node_path || '|' || node_index || '|' || value_type
                                 || '|' || name || '|' || val || '|' || error_code
                             from aux_json_parse(:test_json));
+    expected_value = '"som|lue"|/-/|0|string|some param|text value|0';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -117,8 +122,8 @@ begin
     suspend;
 
     test_name = 'complex: count';
-    expected_value = 16;
     resulting_value = (select count(*) from aux_json_parse(:test_json));
+    expected_value = 16;
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -141,23 +146,23 @@ begin
         },
     }';
     test_name = 'nested obj/arr: d.1.* zero indexes, values and types, and common path';
-    expected_value = '0:-1:number|0:0:number|0:1.0:number|/-/a/b/c/d/-/d.1/-/|3';
     resulting_value = (select node_index || ':' || val || ':' || value_type from aux_json_parse(:test_json) where name = 'd.1.1')
                 || '|' || (select node_index || ':' || val || ':' || value_type  from aux_json_parse(:test_json) where name = 'd.1.2')
                 || '|' || (select node_index || ':' || val || ':' || value_type  from aux_json_parse(:test_json) where name = 'd.1.3')
                 || '|' || (select distinct node_path from aux_json_parse(:test_json) where name starts with 'd.1.')
                 || '|' || (select count(*) from aux_json_parse(:test_json) where node_path = '/-/a/b/c/d/-/d.1/' and value_type = 'object' and val like '"%":%' );
+    expected_value = '0:-1:number|0:0:number|0:1.0:number|/-/a/b/c/d/-/d.1/-/|3';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
 
     test_name = 'nested obj/arr: d.2.* zero indexes, values and types';
-    expected_value = '0:-1.1:number|0:0.3:number|0:1.5:number|/-/a/b/c/d/-/d.2/-/|3';
     resulting_value = (select node_index || ':' || val || ':' || value_type from aux_json_parse(:test_json) where name = 'd.2.1')
                 || '|' || (select node_index || ':' || val || ':' || value_type  from aux_json_parse(:test_json) where name = 'd.2.2')
                 || '|' || (select node_index || ':' || val || ':' || value_type  from aux_json_parse(:test_json) where name = 'd.2.3')
                 || '|' || (select distinct node_path from aux_json_parse(:test_json) where name starts with 'd.2.')
                 || '|' || (select count(*) from aux_json_parse(:test_json) where node_path = '/-/a/b/c/d/-/d.2/' and value_type = 'object' and val like '"%":%');
+    expected_value = '0:-1.1:number|0:0.3:number|0:1.5:number|/-/a/b/c/d/-/d.2/-/|3';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -172,7 +177,6 @@ begin
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
     test_name = 'nested obj/arr: levels';
-    expected_value = '0|1|2|3|4|5|6|7|7|8|8|8';
     resulting_value = (select distinct level from aux_json_parse(:test_json) where node_path = '/') -- 0
                     || '|' || (select distinct level from aux_json_parse(:test_json) where node_path = '/-/') -- 1
                     || '|' || (select distinct level from aux_json_parse(:test_json) where node_path = '/-/a/') -- 2
@@ -186,6 +190,7 @@ begin
                     || '|' || (select distinct level from aux_json_parse(:test_json) where name starts with 'd.1.')
                     || '|' || (select distinct level from aux_json_parse(:test_json) where name starts with 'd.2.')
                     ;
+    expected_value = '0|1|2|3|4|5|6|7|7|8|8|8';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -196,8 +201,8 @@ begin
     -- 1 `array` "d" + 2 `object` inside `array` "d"
     -- 1 `array` "d.1" + 3 `object` inside `array` "d.1" + 3 `string:num` of each item in `array` "d.1"
     -- 1 `array` "d.2" + 3 `object` inside `array` "d.2" + 3 `string:num` of each item in `array` "d.2"
-    expected_value = 21;
     resulting_value = (select count(*) from aux_json_parse(:test_json));
+    expected_value = 21;
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -210,8 +215,8 @@ begin
         "a": [1 2 3]
     }';
     test_name = 'commas between array items';
-    expected_value = '1';
     resulting_value = (select count(*) from aux_json_parse(:test_json) where node_path = '/-/a/' and val = 1 and error_code = 7);
+    expected_value = '1';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;
@@ -224,8 +229,8 @@ begin
         "a": {"z": "y" "f": "e"}
     }';
     test_name = 'commas between array items';
-    expected_value = '1';
     resulting_value = (select count(*) from aux_json_parse(:test_json) where error_code = 7);
+    expected_value = '1';
     test_result = iif(resulting_value is not distinct from expected_value, 1, 0);
     total_count = total_count + 1; success_count = success_count + test_result; summary = success_count || '/' || total_count;
     suspend;

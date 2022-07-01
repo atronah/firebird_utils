484f2e699d64c4da3308ebf6310b2eaac428dcc5
atronah <atronah@users.noreply.github.com>
2022-07-01 19:34:35 +0300

    fixup

--- xml/prc_aux_xml_get_attributes.sql
+++ xml/prc_aux_xml_get_attributes.sql
@@ -38,208 +38,34 @@ begin
 
     attributes = (select trim(match) from mds_aux_regexp_search(:attribute_list_pattern, :xml_openning_tag, 1));
 
-    pos = 0;
-    state = STATE_NONE;
-
+    pos = 1;
     len = char_length(attributes);
+    
     while (pos < len) do
     begin
-        pos = pos + 1;
-        c = substring( from pos for 1);
-
-        state = case state
-                    when STATE_NONE
-                        then iif(c is distinct from ' ', STATE_UNEXPECTED, state)
-                    when STATE_ALIAS
-                        then STATE_UNEXPECTED
-                    when STATE_NAME
-                        then decode(c, ' ', STATE_WS_BEFORE_EQUAL
-                                    , '=', STATE_EQUAL
-                                    , ':', STATE_ALIAS
-                                    , state)
-                    when STATE_EQUAL, STATE_WS_AFTER_EQUAL
-                            , STATE_VALUE, STATE_VALUE
-                            , STATE_UNEXPECTED);
-                    else STATE_UNEXPECTED;
-                end;
-
-        if (state = STATE_ALIAS) then
+        eq_pos = position('=', attributes, pos);
+        
+        if (eq_pos is null) 
+            then break;
+ 
+        colon_pos = position('=', attributes, pos);
+        if (colon_pos between pos and eq_pos) then
         begin
-            if (alias is null) then
-            begin
-                alias = name;
-                name = null;
-                state = STATE_NAME;
-            end
-            else state = STATE_UNEXPECTED;
+            alias = trim(substring(attributes from pos for colon_pos - pos));
+            pos = colon_pos + 1;
         end
-
-        if (state = STATE_UNEXPECTED)
-            then exception ERROR 'parsing error: unexpected character ' || coalesce(c, 'null')
-                                        || ' at position ' || coalesce(pos, 'null');
-
-        if (c = ' ') then
-        begin
-
-        end
-        else if (c = '=') then
-        begin
-            state = decode(state
-                            , STATE_NAME, STATE_EQUAL
-                            , STATE_WS_BEFORE_EQUAL, STATE_EQUAL
-                            , STATE_VALUE, STATE_VALUE
-                            , STATE_UNEXPECTED);
-        end
-        else if (c = '=') then
-        begin
-            state = decode(state
-                            , STATE_NAME, STATE_EQUAL
-                            , STATE_WS_BEFORE_EQUAL, STATE_EQUAL
-                            , STATE_VALUE, STATE_VALUE
-                            , STATE_UNEXPECTED);
-        end
-
-
-
-            if (state in (STATE_NAME, STATE_WHITESPACE))
-                then state = STATE_EQ;
-            else if (state is distinct from STATE_VALUE)
-                then exception ERROR 'parsing error: unexpected character ' || coalesce('"' || c || '"', 'null')
-                                        || ' at position ' || coalesce(pos, 'null');
-        end
-        else if (c = ':') then
-        begin
-            if (state in (STATE_NAME) and alias is null) then
-            begin
-                alias = name;
-                name = null;
-                continue;
-            end else if (state is distinct from STATE_VALUE)
-                then exception ERROR 'parsing error: unexpected character ' || coalesce(c, 'null')
-                                        || ' at position ' || coalesce(pos, 'null');
-        end
-        else if (c = '"') then
-        begin
-            if (state in (STATE_NAME) and alias is null) then
-            begin
-                alias = name;
-                name = null;
-                continue;
-            end else if (state is distinct from STATE_VALUE)
-                then
-        end
-    end
-
-
-
-
-    startpos = position('<', xml);
-    while (startpos > 0) do
-    begin
-        -- returns `<my_node>` for `  <my_node></my_node>`
-        -- returns `<my_node />` for `  <my_node /><a></a>`
-        open_tag = (select match
-                        from mds_aux_regexp_search(:otp
-                                                , substring(:xml from :startpos
-                                                                 for maxvalue(position('>', :xml, :startpos) - :startpos + 1, 0))
-                                                    , 1));
-        if(open_tag is not null) then
-        begin
-            endpos = startpos + char_length(open_tag) - 1; -- position of `>` for open tag
-            -- returns `ns:name` for `<ns:name a="x">`
-            fullname = (select substring(match from 2) from mds_aux_regexp_search('<' || :aliased_name_pattern, :open_tag, 1));
-
-
-            name = substring(fullname from maxvalue(position(':' in fullname) + 1, 0));
-            ns_alias = substring(fullname from 1 for maxvalue(position(name in fullname) - 2, 0));
-
-            if(right(open_tag, 2) <> '/>') then
-            begin
-                nested_level = 0;
-                -- position of the nearest close tag for non-empty node with the same name
-                endpos = position('</' || fullname || '>', xml, endpos);
-                -- get position of the next nearest node with the same name
-                next_similar = startpos;
-                next_similar = position('<' || :fullname || '>', xml, next_similar + 1);
-                next_similar_with_attrs = position('<' || :fullname || ' ', xml, next_similar + 1);
-                if (next_similar_with_attrs > 0 and next_similar_with_attrs < next_similar)
-                    then next_similar = next_similar_with_attrs;
-                -- skip nested nodes with the same name
-                while ((next_similar between startpos and endpos
-                            or nested_level > 0)
-                        and nested_level < NESTED_LIMIT
-                ) do
-                begin
-                    if (next_similar between startpos and endpos) then
-                    begin
-                        nested_level = nested_level + 1;
-                    end
-                    else
-                    begin
-                        nested_level = nested_level - 1;
-                        next_similar = endpos;
-                        endpos = position('</' || fullname || '>', xml, endpos + 1);
-                    end
-
-                    next_similar = position('<' || :fullname || '>', xml, maxvalue(next_similar, startpos) + 1);
-                    next_similar_with_attrs = position('<' || :fullname || ' ', xml, maxvalue(next_similar, startpos) + 1);
-                    if (next_similar_with_attrs > 0 and next_similar_with_attrs < next_similar)
-                        then next_similar = next_similar_with_attrs;
-                end
-
-                content_offset = startpos + char_length(open_tag);
-                content = substring(xml from content_offset
-                                        for maxvalue(endpos - startpos - char_length(open_tag), 0));
-                endpos = endpos + char_length('</' || fullname || '>') - 1;
-            end
-            else
-            begin
-                content = '';
-            end
-            if (endpos < startpos)
-                then exception ERROR 'parsing error: for node ' || coalesce('<' || fullname || '>', 'null')
-                                        || ' end tag found at position ' || coalesce(endpos, 'null')
-                                        || ' that''s before start tag position' || coalesce(startpos, 'null');
-            suspend;
-
-            saved_startpos = startpos;
-            saved_endpos = endpos;
-
-            for select path, ns_uri, ns_alias, name, content, attributes, startpos, endpos, level
-                from aux_xml_all_nodes(:content
-                                        , :root_path || trim(iif(:root_path <> '/', '/', '')) || :name
-                                        , :level + 1)
-                into path, ns_uri, ns_alias, name, content, attributes, startpos, endpos, level
-            do
-            begin
-                startpos = content_offset + startpos - 1;
-                endpos = content_offset + endpos - 1;
-                suspend;
-            end
-            path = root_path;
-            level = root_level;
-            startpos = saved_startpos;
-            endpos = saved_endpos;
-        end
-        else endpos = startpos;
-
-        startpos = position('<', xml, endpos + 1);
+        
+        name = trim(substring(attributes from pos for eq_pos - pos));
+        pos = position('"', attributes, eq_pos + 1) + 1;
+        end_quote_pos = position('"', attributes, pos);
+        val = trim(substring(attributes from pos for end_quote_pos - pos));
+        
+        pos = end_quote_pos;
     end
 end^
 
 set term ; ^
 
 
-comment on procedure aux_xml_all_nodes is 'Parse XML and returns all nodes of it with its pathes and content';
-comment on parameter aux_xml_all_nodes.xml is 'XML data to parse';
-comment on parameter aux_xml_all_nodes.root_path is 'Service input param to pass path in recursive call';
-comment on parameter aux_xml_all_nodes.root_level is 'Service input param to pass level path in recursive call';
-comment on parameter aux_xml_all_nodes.path is 'Path of XML node (`/` for root). Always starts with `/` and do NOT contain `/` at the end';
-comment on parameter aux_xml_all_nodes.ns_uri is 'URI of namespace, declared within node (not yet implemented)';
-comment on parameter aux_xml_all_nodes.ns_alias is 'Alias of node name';
-comment on parameter aux_xml_all_nodes.name is 'Name of XML node';
-comment on parameter aux_xml_all_nodes.content is 'Content of XML node';
-comment on parameter aux_xml_all_nodes.attributes is 'Attributes of XML node';
-comment on parameter aux_xml_all_nodes.startpos is 'Starting position of XML node (position of `<` in openning tag)';
-comment on parameter aux_xml_all_nodes.endpos is 'Ending position of XML node (position of `>` in closing tag)';
-comment on parameter aux_xml_all_nodes.level is 'Hierarchy level of XML node (root has 0 and its chidren has 1, etc.)';
+comment on procedure aux_xml_get_attributes is '';
+comment on parameter aux_xml_get_attributes.xml_openning_tag is '';

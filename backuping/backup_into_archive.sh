5d2de066763294bd408715e3f721404ab6de091b
atronah <atronah.ds@gmail.com>
2022-07-12 17:42:28 +0300

    ft(backuping/backup_into_achive): adds script to fast backup into 7z-archive directly

---
+++ backuping/backup_into_archive.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+gbak_util=/opt/firebird/bin/gbak
+db_user=SYSDBA
+db_pas=masterkey
+
+db_alias=$1
+
+
+if [ -z "$db_alias" ]; then
+    db_alias=box_med
+fi
+
+backup_name="${db_alias}_$(date +%Y-%m-%d_%H%M%S)"
+
+echo "started backup '${db_alias}' into archive '${backup_name}.fbk.7z' with logging into '${backup_name}.fbk.log'"
+"${gbak_util}" -user ${db_user} -pas ${db_pas} -b -g -v ${db_alias} stdout -y ${backup_name}.fbk.log | 7za a -si ${backup_name}.fbk.7z
+
+echo "archiving '${backup_name}.fbk.log' into '${backup_name}.fbk.7z'"
+7za a ${backup_name}.fbk.7z ${backup_name}.fbk.log
+
+echo "done"

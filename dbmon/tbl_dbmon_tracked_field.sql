4e783054c938583587c810fabdc1dc15a09dca0f
atronah <atronah.ds@gmail.com>
2024-06-14 18:01:44 +0300

    fixup! ft(dbmon): adds new table for specify table fields to track changes in it

--- dbmon/tbl_dbmon_tracked_field.sql
+++ dbmon/tbl_dbmon_tracked_field.sql
@@ -1,6 +1,8 @@
 create table dbmon_tracked_field(
     table_name varchar(1024)
     , field_name varchar(1024)
+    , extra_cond varchar(1024)
+    , exclude_roles varchar(1024)
     , enabled smallint
     , update_track_triggers smallint
     , log_call_stack smallint
@@ -13,7 +15,12 @@ After changes you should re-create tracking triggers for table using
 (or pass value `1` to field update_track_triggers)';
 
 comment on column dbmon_tracked_field.table_name is 'Name of table to track data changing in specified field';
-comment on column dbmon_tracked_field.field_name is 'Name of column of table to track data changing in it';
+comment on column dbmon_tracked_field.field_name is 'Name of column of table to track data changing in it.
+Special values:
+- `*` all fields changes will be tracked separatelly
+- `?` changes of any field will by tracked as one log record with `?` in `changed_field_name`';
+comment on column dbmon_tracked_field.extra_cond is 'Extra cond with using fields of `new` and `old` record, to reduce record of tracking';
+comment on column dbmon_tracked_field.exclude_roles is 'Database roles separated by comma for whom tracking is disabled';
 comment on column dbmon_tracked_field.enabled is 'Enables tracking if is not `null`/`0`';
 comment on column dbmon_tracked_field.update_track_triggers is 'If passed 1, triggers for table will be recreated by dbmon_recreate_trigger';
 comment on column dbmon_tracked_field.log_call_stack is 'If passed 1, call stack will be added to logged data of changes';

64e273f57fe772a278534ca7160e14b6039973a9
atronah <atronah.ds@gmail.com>
2024-06-14 13:50:40 +0300

    ft(dbmon): adds trigger to calculate defaults values of new records in new changelog table

---
+++ dbmon/trg_dbmon_structure_changelog_bui.sql
@@ -0,0 +1,47 @@
+set term ^ ;
+create or alter trigger dbmon_structure_changelog_bui
+    active
+    before update or insert
+    on dbmon_structure_changelog
+as
+begin
+    new.changes_id = coalesce(new.changes_id, old.changes_id, next value for dbmon_structure_changelog_seq);
+
+    new.db_name = coalesce(nullif(new.db_name, ''), nullif(old.db_name, ''), rdb$get_context('SYSTEM', 'DB_NAME'));
+
+    new.changed = coalesce(new.changed, old.changed, current_timestamp);
+    new.checked = coalesce(new.checked, old.checked, current_timestamp);
+
+    new.changes_type = upper(coalesce(new.changes_type, old.changes_type, 'unknown'));
+
+    new.client_host = coalesce(new.client_host, old.client_host, rdb$get_context('SYSTEM', 'CLIENT_HOST'));
+    new.client_process = coalesce(new.client_process, old.client_process, rdb$get_context('SYSTEM', 'CLIENT_PROCESS'));
+    new.client_user = coalesce(new.client_user, old.client_user, current_user);
+    new.client_role = coalesce(new.client_role, old.client_role, current_role);
+    new.client_protocol = coalesce(new.client_protocol, old.client_protocol, rdb$get_context('SYSTEM', 'NETWORK_PROTOCOL'));
+    new.client_version = coalesce(new.client_protocol, old.client_protocol, rdb$get_context('SYSTEM', 'NETWORK_PROTOCOL'));
+
+    new.session_id = coalesce(new.session_id, old.session_id, current_connection);
+    new.isolation_level = coalesce(new.isolation_level, old.isolation_level, rdb$get_context('SYSTEM', 'ISOLATION_LEVEL'));
+    new.client_pid = coalesce(new.client_pid, old.client_pid, rdb$get_context('SYSTEM', 'CLIENT_PID'));
+    new.engine_version = coalesce(new.engine_version, old.engine_version, rdb$get_context('SYSTEM', 'ENGINE_VERSION'));
+
+
+    if (nullif(trim(new.client_os_user), '') is null
+            or nullif(trim(new.client_version), '') is null
+            or nullif(trim(new.server_pid), '') is null
+            or nullif(trim(new.auth_method), '') is null
+        ) then
+    begin
+        select
+                coalesce(nullif(trim(new.client_os_user), ''), nullif(trim(a.mon$remote_os_user), ''))
+                , coalesce(nullif(trim(new.client_version), ''), nullif(trim(a.mon$client_version), ''))
+                , coalesce(nullif(trim(new.server_pid), ''), nullif(trim(a.mon$server_pid), ''))
+                , coalesce(nullif(trim(new.auth_method), ''), nullif(trim(a.mon$auth_method), ''))
+            from mon$attachments as a
+            where a.mon$attachment_id = current_connection
+            into new.client_os_user, new.client_version, new.server_pid, new.auth_method;
+    end
+end^
+
+set term ; ^

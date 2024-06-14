152c858a3f5752ca95f721d15a7b894df0949a1e
atronah <atronah.ds@gmail.com>
2024-06-14 13:52:21 +0300

    ft(dbmon): adds trigger to log ddl actions (changes database structure)

---
+++ dbmon/trg_dbmon_before_any_ddl_statement.sql
@@ -0,0 +1,26 @@
+set term ^ ;
+create or alter trigger dbmon_before_any_ddl_statement
+    active
+    before ANY DDL STATEMENT
+as
+begin
+    insert into dbmon_structure_changelog
+            (object_name
+                , object_type
+                , changes_type
+                , old_object_name, new_object_name
+                , sql_text)
+        values (rdb$get_context('DDL_TRIGGER', 'OBJECT_NAME')
+                , rdb$get_context('DDL_TRIGGER', 'OBJECT_TYPE')
+                , rdb$get_context('DDL_TRIGGER', 'EVENT_TYPE')
+                , rdb$get_context('DDL_TRIGGER', 'OLD_OBJECT_NAME')
+                , rdb$get_context('DDL_TRIGGER', 'NEW_OBJECT_NAME')
+                , rdb$get_context('DDL_TRIGGER', 'SQL_TEXT'));
+
+    when any do
+    begin
+        insert into dbmon_structure_changelog (changes_type) values ('error');
+    end
+end^
+
+set term ; ^

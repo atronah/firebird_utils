f4b9228e6263a6379eaaa84d7dfdcc97b1df80d4
atronah <atronah.ds@gmail.com>
2024-06-14 13:53:02 +0300

    ft(dbmon): updates logic of async changes checker by create statement to improved new logic

--- dbmon/prc_dbmon_check_for_changes.sql
+++ dbmon/prc_dbmon_check_for_changes.sql
@@ -1,22 +1,28 @@
 set term ^ ;
 
 create or alter procedure dbmon_check_for_changes(
-    db type of column dbmon_changes_history.db
+    db_connection type of column dbmon_structure_changelog.db_name = null
     , db_user varchar(32)= null
     , db_password varchar(8) = null
     , db_role varchar(32) = null
 )
 as
-declare obj_type type of column dbmon_changes_history.obj_type;
-declare obj_name type of column dbmon_changes_history.obj_name;
-declare prev_create_statement type of column dbmon_changes_history.create_statement;
-declare create_statement type of column dbmon_changes_history.create_statement;
-declare checked type of column dbmon_changes_history.checked;
-declare changed type of column dbmon_changes_history.changed;
+declare db_name type of column dbmon_structure_changelog.db_name;
+declare object_type type of column dbmon_structure_changelog.object_type;
+declare object_name type of column dbmon_structure_changelog.object_name;
+
+declare prev_change_id type of column dbmon_structure_changelog.change_id;
+declare prev_create_statement type of column dbmon_structure_changelog.sql_text;
+
+declare create_statement type of column dbmon_structure_changelog.sql_text;
+declare checked type of column dbmon_structure_changelog.checked;
 declare get_objects_stmt varchar(1024);
+declare CHANGE_TYPE_BY_CREATE_STMT type of column dbmon_structure_changelog.change_type = 'DBMON_CHECK_FOR_CHANGES';
 begin
+    db_name = coalesce(db_connection, rdb$get_context('SYSTEM', 'DB_NAME'));
+
     for select
-            trim(t) as obj_type
+            trim(t) as object_type
             , trim(s) as get_objects_stmt
         from (
             select 'procedure' as t
@@ -36,42 +42,51 @@ begin
                     ' as s
                 from rdb$database
         )
-        into obj_type, get_objects_stmt
+        into object_type, get_objects_stmt
     do
     begin
+        object_type = upper(trim(object_type));
+
         for execute statement get_objects_stmt
-            on external db as user db_user password db_password role db_role
-            into obj_name
+            on external db_connection as user db_user password db_password role db_role
+            into object_name
         do
         begin
-            prev_create_statement = null;
-            changed = null;
-            checked = 'now';
-            create_statement = null;
+            object_name = upper(trim(object_name));
+            checked = cast('now' as timestamp);
 
+            create_statement = null;
             -- aux_get_create_statement - procedure from project github.com/atronah/firebird_utils
             execute statement
-                ('select stmt from aux_get_create_statement(:obj_name)')
-                (obj_name := :obj_name)
-                on external db as user db_user password db_password role db_role
+                ('select stmt from aux_get_create_statement(:object_name)')
+                (object_name := :object_name)
+                on external db_connection as user db_user password db_password role db_role
                 into create_statement;
 
+            prev_change_id = null; prev_create_statement = null;
             select first 1
-                    create_statement
-                    , changed
-                from dbmon_changes_history
-                where db is not distinct from :db
-                    and obj_type is not distinct from :obj_type
-                    and obj_name is not distinct from :obj_name
+                    dsc.change_id
+                    , dsc.sql_text
+                from dbmon_structure_changelog as dsc
+                where dsc.db_name is not distinct from :db_name
+                    and dsc.object_type is not distinct from :object_type
+                    and dsc.object_name is not distinct from :object_name
+                    and dsc.change_type = :CHANGE_TYPE_BY_CREATE_STMT
                 order by checked desc
-                into prev_create_statement, changed;
+                into prev_change_id, prev_create_statement;
 
-            if (prev_create_statement is distinct from create_statement)
-                then changed = checked;
-
-            update or insert into dbmon_changes_history
-                    (db, obj_type, obj_name, changed, checked, create_statement)
-                values (:db, :obj_type, :obj_name, coalesce(:changed, :checked), :checked, :create_statement);
+            if (prev_create_statement is distinct from create_statement) then
+            begin
+                insert into dbmon_structure_changelog
+                    (db_name, object_type, object_name, change_type, changed, checked, sql_text)
+                values (:db_name, :object_type, :object_name, :CHANGE_TYPE_BY_CREATE_STMT, :checked, :checked, :create_statement);
+            end
+            else
+            begin
+                update dbmon_structure_changelog as dsc
+                    set dsc.checked = :checked
+                    where dsc.change_id = :prev_change_id;
+            end
         end
     end
 end^

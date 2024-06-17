ee4533f997cf486109f5c84d819b120d002040a7
atronah <atronah.ds@gmail.com>
2024-06-17 13:45:05 +0300

    ft(dbmon): increase speed of tracking structure changes
    Optimizes get data from `mon$` tables, because access to then requires a lot of time for databases with a lot of connections
    (server send request to each connection and wait answer).
    
    - adds support settings to enable/disable logging some data
    - adds support cache in context variables for values, gotten from mon$tables (rdb$get_context is faster then selet to mon$...)

--- dbmon/trg_dbmon_structure_changelog_bui.sql
+++ dbmon/trg_dbmon_structure_changelog_bui.sql
@@ -4,6 +4,11 @@ create or alter trigger dbmon_structure_changelog_bui
     before update or insert
     on dbmon_structure_changelog
 as
+declare log_attachement_client_os_user smallint;
+declare log_attachement_client_version smallint;
+declare log_attachement_server_pid smallint;
+declare log_attachement_auth_method smallint;
+declare log_context_variables type of column dbmon_settings.val;
 declare context_variable_name type of column mon$context_variables.mon$variable_name;
 declare context_variable_value type of column mon$context_variables.mon$variable_value;
 begin
@@ -24,6 +29,9 @@ begin
     new.client_role = coalesce(new.client_role, old.client_role, current_role);
     new.client_protocol = coalesce(new.client_protocol, old.client_protocol, rdb$get_context('SYSTEM', 'NETWORK_PROTOCOL'));
     new.client_version = coalesce(new.client_protocol, old.client_protocol, rdb$get_context('SYSTEM', 'NETWORK_PROTOCOL'));
+    new.client_os_user = coalesce(new.client_os_user, old.client_os_user, rdb$get_context('USER_SESSION', 'DBMON_CLIENT_OS_USER'));
+    new.server_pid = coalesce(new.server_pid, old.server_pid, rdb$get_context('USER_SESSION', 'DBMON_SERVER_PID'));
+    new.auth_method = coalesce(new.auth_method, old.auth_method, rdb$get_context('USER_SESSION', 'DBMON_AUTH_METHOD'));
 
     new.session_id = coalesce(new.session_id, old.session_id, current_connection);
     new.transaction_id = coalesce(new.transaction_id, old.transaction_id, current_transaction);
@@ -31,10 +39,14 @@ begin
     new.client_pid = coalesce(new.client_pid, old.client_pid, rdb$get_context('SYSTEM', 'CLIENT_PID'));
     new.engine_version = coalesce(new.engine_version, old.engine_version, rdb$get_context('SYSTEM', 'ENGINE_VERSION'));
 
-    if (nullif(trim(new.client_os_user), '') is null
-            or nullif(trim(new.client_version), '') is null
-            or nullif(trim(new.server_pid), '') is null
-            or nullif(trim(new.auth_method), '') is null
+    log_attachement_client_os_user = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_client_os_user');
+    log_attachement_client_version = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_client_version');
+    log_attachement_server_pid = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_server_pid');
+    log_attachement_auth_method = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_auth_method');
+    if (nullif(trim(new.client_os_user), '') is null and log_attachement_client_os_user > 0
+            or nullif(trim(new.client_version), '') is null and log_attachement_client_version > 0
+            or nullif(trim(new.server_pid), '') is null and log_attachement_server_pid > 0
+            or nullif(trim(new.auth_method), '') is null and log_attachement_auth_method > 0
         ) then
     begin
         select
@@ -45,27 +57,49 @@ begin
             from mon$attachments as a
             where a.mon$attachment_id = current_connection
             into new.client_os_user, new.client_version, new.server_pid, new.auth_method;
+
+        if (new.client_os_user is not null)
+            then rdb$set_context('USER_SESSION', 'DBMON_CLIENT_OS_USER', new.client_os_user);
+        if (new.client_os_user is not null)
+            then rdb$set_context('USER_SESSION', 'DBMON_SERVER_PID', new.server_pid);
+        if (new.client_os_user is not null)
+            then rdb$set_context('USER_SESSION', 'DBMON_AUTH_METHOD', new.auth_method);
     end
 
-    if (new.context_variables is null) then
+
+    log_context_variables = (select val from dbmon_settings where key = 'log_context_variables');
+    if (new.context_variables is null and log_context_variables > '') then
     begin
         new.context_variables = '';
-
-        for select distinct
-              mon$variable_name, mon$variable_value
-          from mon$context_variables
-          where mon$attachment_id = current_connection
-              or mon$transaction_id = rdb$get_context('SYSTEM', 'TRANSACTION_ID')
-          order by 1
-          into context_variable_name, context_variable_value
+        for select part
+            from aux_split_text(:log_context_variables, ';')
+            where part containing '.'
+            into context_variable_name
         do
         begin
-           new.context_variables = left(new.context_variables
+            context_variable_value = rdb$get_context(trim(substring(context_variable_name
+                                                                    from 1
+                                                                    for position('.' in context_variable_name) - 1))
+                                                    , trim(substring(context_variable_name
+                                                                    from position('.' in context_variable_name) + 1))
+                                                    );
+
+            new.context_variables = left(new.context_variables
                                         || coalesce(context_variable_name, 'null')
                                         || '='
                                         || coalesce(context_variable_value, 'null')
                                         || ascii_char(13) || ascii_char(10)
                                     , 4096);
+
+            when any do
+            begin
+                new.context_variables = left(new.context_variables
+                                                || 'Exception when getting value of variable "'
+                                                || coalesce(context_variable_name, 'null')
+                                                || '"'
+                                                || ascii_char(13) || ascii_char(10)
+                                            , 4096);
+            end
         end
     end
 

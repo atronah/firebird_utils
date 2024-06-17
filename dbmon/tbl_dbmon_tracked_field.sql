097cf49acc8c35e8a5d9ac5be3205bc49831583c
atronah <atronah.ds@gmail.com>
2024-06-17 13:45:51 +0300

    ft(dbmon): removes `log_context_variables` setting for each track field
    replaced by common settings from `dbmon_settings`

--- dbmon/tbl_dbmon_tracked_field.sql
+++ dbmon/tbl_dbmon_tracked_field.sql
@@ -6,7 +6,6 @@ create table dbmon_tracked_field(
     , exclude_roles varchar(1024)
     , update_track_triggers smallint
     , log_call_stack smallint
-    , log_context_variables smallint
     , constraint pk_dbmon_tracked_field primary key (table_name, field_name)
 );
 
@@ -26,4 +25,3 @@ comment on column dbmon_tracked_field.extra_cond is 'Extra cond with using field
 comment on column dbmon_tracked_field.exclude_roles is 'Database roles separated by comma for whom tracking is disabled';
 comment on column dbmon_tracked_field.update_track_triggers is 'If passed 1, triggers for table will be recreated by dbmon_create_triggers';
 comment on column dbmon_tracked_field.log_call_stack is 'If passed 1, call stack will be added to logged data of changes';
-comment on column dbmon_tracked_field.log_context_variables is 'If passed 1, context variables will be added to logged data of changes';

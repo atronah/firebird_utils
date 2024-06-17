e53938207532b38a7064a0037903f10326a72bd6
atronah <atronah.ds@gmail.com>
2024-06-17 11:14:58 +0300

    fixup! fixup! ft(dbmon): adds trigger to re-create triggers if `update_track_triggers=1` passed

--- dbmon/trg_dbmon_tracked_field_aui.sql
+++ dbmon/trg_dbmon_tracked_field_aui.sql
@@ -4,10 +4,12 @@ create or alter trigger dbmon_tracked_field_aui
     after update or insert
     on dbmon_tracked_field
 as
+declare count_of_created_triggers bigint;
 begin
     if (new.update_track_triggers = 1) then
     begin
-        execute procedure dbmon_create_triggers(new.table_name, 1);
+        count_of_created_triggers = (select count_of_created_triggers
+                                        from dbmon_create_triggers(new.table_name, 1));
 
         update dbmon_tracked_field
             set update_track_triggers = 0

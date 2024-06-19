be00d03da9e5f8f2bb4a4cd2c603de9d5c6b8f5a
atronah <atronah.ds@gmail.com>
2024-06-19 14:40:44 +0300

    fixup! ft(dbmon): increase speed of tracking structure changes

--- dbmon/trg_dbmon_structure_changelog_bui.sql
+++ dbmon/trg_dbmon_structure_changelog_bui.sql
@@ -4,10 +4,10 @@ create or alter trigger dbmon_structure_changelog_bui
     before update or insert
     on dbmon_structure_changelog
 as
-declare log_attachement_client_os_user type of column dbmon_settings.val;
-declare log_attachement_client_version type of column dbmon_settings.val;
-declare log_attachement_server_pid type of column dbmon_settings.val;
-declare log_attachement_auth_method type of column dbmon_settings.val;
+declare log_attachement_client_os_user smallint;
+declare log_attachement_client_version smallint;
+declare log_attachement_server_pid smallint;
+declare log_attachement_auth_method smallint;
 declare log_context_variables type of column dbmon_settings.val;
 declare context_variable_name type of column mon$context_variables.mon$variable_name;
 declare context_variable_value type of column mon$context_variables.mon$variable_value;
@@ -38,10 +38,10 @@ begin
     new.client_pid = coalesce(new.client_pid, old.client_pid, rdb$get_context('SYSTEM', 'CLIENT_PID'));
     new.engine_version = coalesce(new.engine_version, old.engine_version, rdb$get_context('SYSTEM', 'ENGINE_VERSION'));
 
-    log_attachement_client_os_user = (select val from dbmon_settings where key = 'log_attachement_client_os_user');
-    log_attachement_client_version = (select val from dbmon_settings where key = 'log_attachement_client_version');
-    log_attachement_server_pid = (select val from dbmon_settings where key = 'log_attachement_server_pid');
-    log_attachement_auth_method = (select val from dbmon_settings where key = 'log_attachement_auth_method');
+    log_attachement_client_os_user = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_client_os_user');
+    log_attachement_client_version = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_client_version');
+    log_attachement_server_pid = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_server_pid');
+    log_attachement_auth_method = (select iif(val similar to '0|1', val, 0) from dbmon_settings where key = 'log_attachement_auth_method');
     if (nullif(trim(new.client_os_user), '') is null and log_attachement_client_os_user > 0
             or nullif(trim(new.client_version), '') is null and log_attachement_client_version > 0
             or nullif(trim(new.server_pid), '') is null and log_attachement_server_pid > 0

d4a9b9291271dfe3134e9f2c3a52c833a40ea9d4
atronah <atronah.ds@gmail.com>
2025-08-13 15:51:31 +0300

    rf(dbmon): some small improvements for trigger `dbmon_tracked_field_aui`
    - adds alias for table in select statement
    - adds check for changing value of field `dbmon_tracked_field.update_track_triggers` for processing creating triggers

--- dbmon/trg_dbmon_tracked_field_aui.sql
+++ dbmon/trg_dbmon_tracked_field_aui.sql
@@ -9,15 +9,17 @@ begin
     -- author: atronah (look for me by this nickname on GitHub and GitLab)
     -- source: https://github.com/atronah/firebird_utils/tree/master/dbmon
 
-    if (new.update_track_triggers = 1) then
+    if (new.update_track_triggers = 1
+        and old.update_track_triggers is distinct from new.update_track_triggers
+    ) then
     begin
         count_of_created_triggers = (select count_of_created_triggers
                                         from dbmon_create_triggers(new.table_name, 1));
 
-        update dbmon_tracked_field
-            set update_track_triggers = 0
-            where table_name = new.table_name
-                and field_name = new.field_name;
+        update dbmon_tracked_field as tf
+            set tf.update_track_triggers = 0
+            where tf.table_name = new.table_name
+                and tf.field_name = new.field_name;
     end
 end^
 

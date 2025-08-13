a746eba44ad86d947b04041265c1a66c7cda28e4
atronah <atronah.ds@gmail.com>
2025-08-13 15:55:23 +0300

    ft(dbmon): adds field `errors` into table `dbmon_tracked_field`
    to store errors of field description (like `Table not found`)

--- dbmon/tbl_dbmon_tracked_field.sql
+++ dbmon/tbl_dbmon_tracked_field.sql
@@ -6,6 +6,7 @@ create table dbmon_tracked_field(
     , exclude_roles varchar(1024)
     , update_track_triggers smallint
     , log_call_stack smallint
+    , errors varchar(1024)
     , constraint pk_dbmon_tracked_field primary key (table_name, field_name)
 );
 
@@ -25,3 +26,4 @@ comment on column dbmon_tracked_field.extra_cond is 'Extra cond with using field
 comment on column dbmon_tracked_field.exclude_roles is 'Database roles separated by comma for whom tracking is disabled';
 comment on column dbmon_tracked_field.update_track_triggers is 'If passed 1, triggers for table will be recreated by dbmon_create_triggers';
 comment on column dbmon_tracked_field.log_call_stack is 'If passed 1, call stack will be added to logged data of changes';
+comment on column dbmon_tracked_field.errors is 'Description of the error that occurred while processing changes';

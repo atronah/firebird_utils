14b83fac9822913645fe45659c31e36542d3a967
atronah <atronah.ds@gmail.com>
2025-08-13 16:05:06 +0300

    ft(dbmon): improves ddl-trigger
    - adds processing changes block (from new table `dbmon_block_stucture_changes`)
    - removes common `when any do` block, because raising exception required to prevent (block) changing database object
    - changes logic of logging errors (previous all errors logged into the same record of `dbmon_structure_changelog` with `change_id = 1`)

--- dbmon/trg_dbmon_before_any_ddl_statement.sql
+++ dbmon/trg_dbmon_before_any_ddl_statement.sql
@@ -3,73 +3,86 @@ create or alter trigger dbmon_before_any_ddl_statement
     active
     before ANY DDL STATEMENT
 as
+declare object_type type of column dbmon_structure_changelog.object_type;
+declare object_name type of column dbmon_structure_changelog.object_name;
+declare block_enabled type of column dbmon_block_stucture_changes.enabled;
+declare block_comment type of column dbmon_block_stucture_changes.comment;
 declare prev_unified_create_statement type of column dbmon_structure_changelog.prev_unified_create_statement;
 begin
     -- author: atronah (look for me by this nickname on GitHub and GitLab)
     -- source: https://github.com/atronah/firebird_utils/tree/master/dbmon
 
-    if ((select val from dbmon_settings where key = 'log_prev_unified_create_statement') > 0) then
+    object_type = upper(trim(rdb$get_context('DDL_TRIGGER', 'OBJECT_TYPE')));
+    object_name = upper(trim(rdb$get_context('DDL_TRIGGER', 'OBJECT_NAME')));
+
+    select
+            bsc.enabled, bsc.comment
+        from dbmon_block_stucture_changes as bsc
+        where bsc.object_type = :object_type
+            and bsc.object_name = :object_name
+        into block_enabled, block_comment;
+    block_enabled = coalesce(block_enabled, 0);
+
+    if (block_enabled is distinct from 0) then
     begin
-        if (exists(select rdb$procedure_name
-                    from rdb$procedures
-                    where rdb$package_name is null
-                        and rdb$procedure_name = upper('aux_get_create_statement'))
-        ) then
-        begin
-            execute statement ('select stmt from aux_get_create_statement(:object_name, :object_type)')
-                (object_name := rdb$get_context('DDL_TRIGGER', 'OBJECT_NAME')
-                , object_type := rdb$get_context('DDL_TRIGGER', 'OBJECT_TYPE'))
-                into prev_unified_create_statement;
-        end
-        else
-        begin
-            insert into dbmon_structure_changelog (change_id, change_type, change_comment)
-                values (1, 'error'
-                        , 'Procedure aux_get_create_statement doesn''t exists in database.'
-                        || ' Settings log_prev_unified_create_statement has ben disabled'
-                        ||'; SQLCODE=' || coalesce(SQLCODE, 'null')
-                        ||'; GDSCODE=' || coalesce(GDSCODE, 'null')
-                        ||'; SQLSTATE=' || coalesce(SQLSTATE, 'null')
-                        );
-            update dbmon_settings set val = 0 where key = 'log_prev_unified_create_statement';
-        end
-
-
-        when any do
-        begin
-            insert into dbmon_structure_changelog (change_id, change_type, change_comment)
-                values (1, 'error'
-                        , 'Exception during uring computing prev_unified_create_statement'
-                        ||'; SQLCODE=' || coalesce(SQLCODE, 'null')
-                        ||'; GDSCODE=' || coalesce(GDSCODE, 'null')
-                        ||'; SQLSTATE=' || coalesce(SQLSTATE, 'null')
-                        );
-        end
+        exception DBMON_CHANGES_NOT_ALLOWED coalesce(block_comment, '');
     end
-
-    insert into dbmon_structure_changelog
-            (object_name
-                , object_type
-                , change_type
-                , old_object_name, new_object_name
-                , sql_text
-                , prev_unified_create_statement)
-        values (rdb$get_context('DDL_TRIGGER', 'OBJECT_NAME')
-                , rdb$get_context('DDL_TRIGGER', 'OBJECT_TYPE')
-                , rdb$get_context('DDL_TRIGGER', 'EVENT_TYPE')
-                , rdb$get_context('DDL_TRIGGER', 'OLD_OBJECT_NAME')
-                , rdb$get_context('DDL_TRIGGER', 'NEW_OBJECT_NAME')
-                , rdb$get_context('DDL_TRIGGER', 'SQL_TEXT')
-                , :prev_unified_create_statement);
-
-    when any do
+    else
     begin
-        update or insert into dbmon_structure_changelog (change_id, change_type, change_comment)
-            values (1, 'error'
-                    , 'SQLCODE=' || coalesce(SQLCODE, 'null')
-                    ||'; GDSCODE=' || coalesce(GDSCODE, 'null')
-                    ||'; SQLSTATE=' || coalesce(SQLSTATE, 'null')
-                    );
+        if ((select val from dbmon_settings where key = 'log_prev_unified_create_statement') > 0) then
+        begin
+            if (exists(select rdb$procedure_name
+                        from rdb$procedures
+                        where rdb$package_name is null
+                            and rdb$procedure_name = upper('aux_get_create_statement'))
+            ) then
+            begin
+                execute statement ('select stmt from aux_get_create_statement(:object_name, :object_type)')
+                    (object_name := :object_name
+                    , object_type := :object_type)
+                    into prev_unified_create_statement;
+            end
+            else
+            begin
+                insert into dbmon_structure_changelog
+                    (object_name, object_type, change_type, change_comment)
+                values (:object_name, :object_type, 'ERROR'
+                        , 'Procedure aux_get_create_statement doesn''t exists in database.'
+                            || ' Settings log_prev_unified_create_statement has ben disabled'
+                            ||'; SQLCODE=' || coalesce(SQLCODE, 'null')
+                            ||'; GDSCODE=' || coalesce(GDSCODE, 'null')
+                            ||'; SQLSTATE=' || coalesce(SQLSTATE, 'null')
+                );
+                update dbmon_settings set val = 0 where key = 'log_prev_unified_create_statement';
+            end
+
+            when any do
+            begin
+                insert into dbmon_structure_changelog
+                    (object_name, object_type, change_type, change_comment)
+                values (:object_name, :object_type, 'ERROR'
+                        , 'Exception during uring computing prev_unified_create_statement'
+                            ||'; SQLCODE=' || coalesce(SQLCODE, 'null')
+                            ||'; GDSCODE=' || coalesce(GDSCODE, 'null')
+                            ||'; SQLSTATE=' || coalesce(SQLSTATE, 'null')
+                );
+            end
+        end
+
+        insert into dbmon_structure_changelog
+                (object_name
+                    , object_type
+                    , change_type
+                    , old_object_name, new_object_name
+                    , sql_text
+                    , prev_unified_create_statement)
+            values (:object_name
+                    , :object_type
+                    , rdb$get_context('DDL_TRIGGER', 'EVENT_TYPE')
+                    , rdb$get_context('DDL_TRIGGER', 'OLD_OBJECT_NAME')
+                    , rdb$get_context('DDL_TRIGGER', 'NEW_OBJECT_NAME')
+                    , rdb$get_context('DDL_TRIGGER', 'SQL_TEXT')
+                    , :prev_unified_create_statement);
     end
 end^
 

e0f8b06d09a5daa22722ff5b387c6c47ab11e771
atronah <atronah.ds@gmail.com>
2026-01-14 16:09:27 +0300

    fx(aux_get_dependencies): some improvements and fixes
    - add support db objects with the same name and different type
    (for example, one database can containg domain `A` and procedure `A`, and previous logic faild when try to get required objects with name `A`)
    - fixes `object_type` value for column (it should be equal `object_type` of table, i.e. `0`), but it was `9` as for domain
    - fixes `object_type` value for domain (it should be equal `9`), but it was `-1`
    - renames `procedure param` to `parameter`

--- prc_aux_get_dependencies.sql
+++ prc_aux_get_dependencies.sql
@@ -7,27 +7,37 @@ create or alter procedure aux_get_dependencies(
     , max_depth smallint = null
 )
 returns(
-    object_name type of column rdb$dependencies.rdb$dependent_name
-    , object_column type of column rdb$dependencies.rdb$field_name
+    object_name varchar(255)
+    , object_column varchar(255)
     , object_type type of column rdb$dependencies.rdb$dependent_type
     , object_type_name varchar(16)
     , depth smallint
 )
 as
 declare object_full_name varchar(255);
+declare object_type_filter type of column rdb$dependencies.rdb$dependent_type;
 declare next_objects_to_process varchar(32000);
 declare processed_objects varchar(32000);
-declare required_object_name type of column rdb$dependencies.rdb$depended_on_name;
-declare required_object_column type of column rdb$dependencies.rdb$field_name;
+declare required_object_name varchar(255);
+declare required_object_column varchar(255);
 declare required_object_type type of column rdb$dependencies.rdb$depended_on_type;
 declare TYPE_TABLE type of column rdb$dependencies.rdb$dependent_type = 0;
+declare TYPE_TABLE_NAME varchar(16) = 'table';
 declare TYPE_VIEW type of column rdb$dependencies.rdb$dependent_type = 1;
+declare TYPE_VIEW_NAME varchar(16) = 'view';
 declare TYPE_TRIGGER type of column rdb$dependencies.rdb$dependent_type = 2;
+declare TYPE_TRIGGER_NAME varchar(16) = 'trigger';
 declare TYPE_PROCEDURE type of column rdb$dependencies.rdb$dependent_type = 5;
+declare TYPE_PROCEDURE_NAME varchar(16) = 'procedure';
+declare TYPE_PROCEDURE_PARAM_NAME varchar(16) = 'parameter';
 declare TYPE_EXCEPTION type of column rdb$dependencies.rdb$dependent_type = 7;
-declare TYPE_COLUMN type of column rdb$dependencies.rdb$dependent_type = 9;
+declare TYPE_EXCEPTION_NAME varchar(16) = 'exception';
+declare TYPE_COLUMN type of column rdb$dependencies.rdb$dependent_type = 0;
+declare TYPE_COLUMN_NAME varchar(16) = 'column';
 declare TYPE_SEQUENCE type of column rdb$dependencies.rdb$dependent_type = 14;
-declare TYPE_DOMAIN type of column rdb$dependencies.rdb$dependent_type = -1;
+declare TYPE_SEQUENCE_NAME varchar(16) = 'sequence';
+declare TYPE_DOMAIN type of column rdb$dependencies.rdb$dependent_type = 9;
+declare TYPE_DOMAIN_NAME varchar(16) = 'domain';
 begin
     -- author: atronah (look for me by this nickname on GitHub and GitLab)
     -- source: https://github.com/atronah/firebird_utils
@@ -43,15 +53,15 @@ begin
 
     types_to_exclude = (select
                             list(distinct decode(lower(trim(part))
-                                                , 'table', :TYPE_TABLE
-                                                , 'view', :TYPE_VIEW
-                                                , 'trigger', :TYPE_TRIGGER
-                                                , 'procedure param', :TYPE_PROCEDURE
-                                                , 'procedure', :TYPE_PROCEDURE
-                                                , 'exception', :TYPE_EXCEPTION
-                                                , 'column', :TYPE_COLUMN
-                                                , 'sequence', :TYPE_SEQUENCE
-                                                , 'domain', :TYPE_DOMAIN
+                                                , :TYPE_TABLE_NAME, :TYPE_TABLE
+                                                , :TYPE_VIEW_NAME, :TYPE_VIEW
+                                                , :TYPE_TRIGGER_NAME, :TYPE_TRIGGER
+                                                , :TYPE_PROCEDURE_PARAM_NAME, :TYPE_PROCEDURE
+                                                , :TYPE_PROCEDURE_NAME, :TYPE_PROCEDURE
+                                                , :TYPE_EXCEPTION_NAME, :TYPE_EXCEPTION
+                                                , :TYPE_COLUMN_NAME, :TYPE_COLUMN
+                                                , :TYPE_SEQUENCE_NAME, :TYPE_SEQUENCE
+                                                , :TYPE_DOMAIN_NAME, :TYPE_DOMAIN
                                                 , lower(trim(part))))
                         from aux_split_text(:types_to_exclude));
     types_to_exclude = coalesce(types_to_exclude, '');
@@ -61,121 +71,134 @@ begin
         for select distinct
                 iif(part containing '.'
                     , substring(part from 1 for position('.' in part) - 1)
-                    , part) as object_name
+                    , part
+                ) as object_name
                 , iif(part containing '.'
                     , substring(part from position('.' in part) + 1)
-                    , null) as object_column
+                    , null
+                ) as object_column
             from aux_split_text(:objects_to_process, ',')
             where trim(part) not similar to :objects_to_exclude_regex
                 and (',' || :processed_objects || ',') not like ('%,' || hash(upper(trim(part))) || ',%')
             into object_name, object_column
         do
         begin
+            object_name = upper(trim(object_name));
+            object_column = upper(trim(object_column));
+            object_type_filter = null;
             object_type = null;
             object_type_name = null;
 
-            with info as (
-                select
-                    :TYPE_PROCEDURE as object_type
-                from rdb$procedures
-                where rdb$procedure_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    :TYPE_TRIGGER as object_type
-                from rdb$triggers
-                where rdb$trigger_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    :TYPE_COLUMN as object_type
-                from rdb$relation_fields
-                where rdb$relation_name = upper(trim(:object_name))
-                    and rdb$field_name = upper(trim(:object_column))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    decode(rdb$relation_type
-                            , 1, :TYPE_VIEW
-                            , :TYPE_TABLE
-                    ) as object_type
-                from rdb$relations
-                where nullif(:object_column, '') is null
-                    and rdb$relation_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    :TYPE_EXCEPTION as object_type
-                from rdb$exceptions
-                where rdb$exception_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    :TYPE_DOMAIN as object_type
-                from rdb$fields
-                where rdb$field_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-                union
-                select
-                    :TYPE_SEQUENCE as object_type
-                from rdb$generators
-                where rdb$generator_name = upper(trim(:object_name))
-                    and coalesce(rdb$system_flag, 0) = 0
-            )
-            select object_type
-                from info
-                into object_type;
-
-            if (object_type is not null) then
+            if (object_name containing ':') then
             begin
-                object_type_name = trim(decode(object_type
-                                            , :TYPE_TABLE, 'table'
-                                            , :TYPE_VIEW, 'view'
-                                            , :TYPE_TRIGGER, 'trigger'
-                                            , :TYPE_PROCEDURE, iif(object_column is not null
-                                                                    , 'procedure param'
-                                                                    , 'procedure')
-                                            , :TYPE_EXCEPTION, 'exception'
-                                            , :TYPE_COLUMN, 'column'
-                                            , :TYPE_SEQUENCE, 'sequence'
-                                            , :TYPE_DOMAIN, 'domain'
-                                            , 'unknown(' || object_type || ')'
-                                        )
-                                    );
+                object_type_filter = substring(object_name from 1 for position(':' in object_name) - 1);
+                object_name = substring(object_name from position(':' in object_name) + 1);
+            end
 
-                object_full_name = upper(trim(object_name)
-                                        || trim(coalesce('.' || trim(object_column), '')));
+            for with info as (
+                    select
+                        :TYPE_PROCEDURE as object_type
+                        , iif(nullif(:object_column, '') is null
+                                , :TYPE_PROCEDURE_NAME
+                                , :TYPE_PROCEDURE_PARAM_NAME
+                        ) as object_type_name
+                    from rdb$procedures
+                    where rdb$procedure_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        :TYPE_TRIGGER as object_type, :TYPE_TRIGGER_NAME as object_type_name
+                    from rdb$triggers
+                    where rdb$trigger_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        :TYPE_COLUMN as object_type, :TYPE_COLUMN_NAME as object_type_name
+                    from rdb$relation_fields
+                    where rdb$relation_name = upper(trim(:object_name))
+                        and rdb$field_name = upper(trim(:object_column))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        iif(rdb$relation_type = 1, :TYPE_VIEW, :TYPE_TABLE) as object_type
+                        , iif(rdb$relation_type = 1, :TYPE_VIEW_NAME, :TYPE_TABLE_NAME) as object_type_name
+                    from rdb$relations
+                    where nullif(:object_column, '') is null
+                        and rdb$relation_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        :TYPE_EXCEPTION as object_type, :TYPE_EXCEPTION_NAME as object_type_name
+                    from rdb$exceptions
+                    where rdb$exception_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        :TYPE_DOMAIN as object_type, :TYPE_DOMAIN_NAME as object_type_name
+                    from rdb$fields
+                    where rdb$field_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                    union
+                    select
+                        :TYPE_SEQUENCE as object_type, :TYPE_SEQUENCE_NAME as object_type_name
+                    from rdb$generators
+                    where rdb$generator_name = upper(trim(:object_name))
+                        and coalesce(rdb$system_flag, 0) = 0
+                )
+                select
+                    info.object_type, trim(info.object_type_name)
+                from info
+                where info.object_type = coalesce(:object_type_filter, info.object_type)
+                into object_type, object_type_name
+            do
+            begin
+                object_full_name = trim(object_type || ':'
+                                        || object_name
+                                        || coalesce('.' || object_column, ''));
                 if ((',' || :processed_objects || ',') not like ('%,' || hash(object_full_name) || ',%'))
                     then processed_objects = processed_objects || ',' || hash(object_full_name);
 
                 suspend;
 
-                for select
+                -- get list of db objects, which required for current object (with :object_name and :object_type)
+                for -- general dependencies
+                    select
                         rdb$depended_on_name as required_object_name
                         , rdb$field_name as required_object_column
                         , rdb$depended_on_type as required_object_type
                     from rdb$dependencies
-                    where rdb$dependent_name = upper(trim(:object_name))
+                    where rdb$dependent_name = :object_name
+                        and rdb$dependent_type = :object_type
                         and trim(rdb$depended_on_name) || trim(coalesce('.' || trim(rdb$field_name), '')) not similar to :objects_to_exclude_regex
-                        and (',' || :processed_objects || ',') not like ('%,' || hash(upper(trim(rdb$depended_on_name) || trim(coalesce('.' || trim(rdb$field_name), '')))) || ',%')
+                        and (',' || :processed_objects || ',')
+                                not like ('%,' || hash(rdb$depended_on_type || ':'
+                                                        || upper(trim(rdb$depended_on_name)
+                                                        || trim(coalesce('.' || trim(rdb$field_name), '')))) || ',%')
                         and (',' || :types_to_exclude || ',') not like ('%,' || rdb$depended_on_type || ',%')
                     union
+                    -- all domains used for table columns
                     select
                         rdb$field_source as required_object_name
                         , null as required_object_column
                         , :TYPE_DOMAIN as required_object_type
                     from rdb$relation_fields
                     where rdb$relation_name = upper(trim(:object_name))
+                        and :object_type = :TYPE_TABLE
                         and rdb$field_name = upper(trim(coalesce(:object_column, rdb$field_name)))
                         and rdb$field_source not starts with 'RDB$'
                         and trim(rdb$relation_name) not similar to :objects_to_exclude_regex
-                        and (',' || :processed_objects || ',') not like ('%,' || hash(upper(trim(rdb$field_source))) || ',%')
+                        and (',' || :processed_objects || ',')
+                                not like ('%,' || hash(upper(trim(:TYPE_DOMAIN || ':' || rdb$field_source))) || ',%')
                         and (',' || :types_to_exclude || ',') not like ('%,' || :TYPE_DOMAIN || ',%')
                     into required_object_name, required_object_column, required_object_type
                 do
                 begin
-                    object_full_name = upper(trim(required_object_name)
-                                            || trim(coalesce('.' || trim(required_object_column), '')));
+                    required_object_name = upper(trim(required_object_name));
+                    required_object_column = upper(trim(required_object_column));
+
+                    object_full_name =  trim(required_object_type || ':'
+                                            || required_object_name
+                                            || coalesce('.' || required_object_column, ''));
                     if ((',' || next_objects_to_process || ',') not like ('%,' || object_full_name || ',%')) then
                     begin
                         next_objects_to_process = next_objects_to_process || ',' || object_full_name;
